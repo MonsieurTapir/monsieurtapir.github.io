76ec47eec7ab6aefb8ef665dd0c182915c42e26e
tapir <pollet.valentin@gmail.com>
2019-07-21 13:40:49 +0200

    fixed pushpin o/

--- js/init.js
+++ js/init.js
@@ -1,25 +1,42 @@
+function offset(elt) {
+        var rect = elt.getBoundingClientRect(), bodyElt = document.body;
+        var sct=window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+        var scl=window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0
+        return {
+            top: rect.top + sct,
+            left: rect.left + scl
+        }
+    }
+
 document.addEventListener('DOMContentLoaded', function() {
+  window.scrollTo(0,0);
   var elems = document.querySelectorAll('#left-one');
   var instances = M.Sidenav.init(elems, {});
 
- elems = document.querySelectorAll('#right-one');
- instances = M.Sidenav.init(elems, {edge:'right',draggable:false});
+  elems = document.querySelectorAll('#right-one');
+  instances = M.Sidenav.init(elems, {
+    edge: 'right',
+    draggable: false
+  });
 
- elems = document.querySelectorAll('.scrollspy');
- instances = M.ScrollSpy.init(elems, {scrollOffset:0});
+  elems = document.querySelectorAll('.scrollspy');
+  instances = M.ScrollSpy.init(elems, {
+    scrollOffset: 0
+  });
+
+  elems = document.querySelectorAll('.collapsible');
+  instances = M.Collapsible.init(elems, {});
+
+  elems = document.querySelectorAll('.pushpin');
+  instances = M.Pushpin.init(elems, {});
+
+  elems = document.querySelectorAll('.pushpin-title');
+  elems.forEach(function(currentValue){
+    var tar=document.querySelector('#'+currentValue.getAttribute('data-target'));
+    M.Pushpin.init(currentValue,{
+      top: offset(tar).top,
+      bottom: offset(tar).top + tar.offsetHeight - currentValue.offsetHeight
+    });
+  });
 
 });
-
-$(document).ready(function(){
-  $('.pushpin').pushpin();
-/*  $('.pushpin-head').each(function() {
-   var $this = $(this);
-   var $target = $('#' + $(this).attr('data-target'));
-   $this.pushpin({
-     top: $target.offset().top,
-     bottom: $target.offset().top + $target.outerHeight() - $this.height()
-   });
- });
-
- */
- });
